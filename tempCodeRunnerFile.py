605fa2099e232273265d59d60c0263598539fb23
jmikelel <161890739+jmikelel@users.noreply.github.com>
2024-04-05 01:29:31 -0500

    parametros de calibracion

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,4 +1,4 @@
-python monocular-camera-calibration.py --path_to_calibration_images sample-calibration-images/ \
+    python monocular-camera-calibration.py --path_to_calibration_images sample-calibration-images/ \
     --calibration_image_format JPG \
     --chessboard_size 9 6 \
     --output_calibration_parameters calibration-parameters/calibration_data.json
